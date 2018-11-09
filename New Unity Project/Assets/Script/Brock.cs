873a272e3c596837349ed5b1f8c00901e474322f
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-11-09 14:36:14 +0900

    プレイヤの左右移動・コントローラへの対応に完了

---
+++ New Unity Project/Assets/Script/Brock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brock : MonoBehaviour {
+
+    SpriteRenderer MainSpriteRenderer;
+    Collider2D col;
+
+    public Sprite WhiteSprite;
+    public Sprite BrackSprite;
+
+    void Start()
+    {
+        // このobjectのSpriteRenderer,Colliderを取得
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        col = gameObject.GetComponent<Collider2D>();
+    }
+
+    public void Blackor(bool a)
+    {
+        if (a == true)
+        {
+            MainSpriteRenderer.sprite = BrackSprite;
+            col.enabled = true;
+        }
+    }
+    public void Whitecor(bool a)
+    {
+        if (a == false)
+        {
+            MainSpriteRenderer.sprite = WhiteSprite;
+            col.enabled = false;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Script/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    GameObject Pc;
+    Vector2 playvec;
+    public float Speed = 0.05f;
+    // Use this for initialization
+    void Start()
+    {
+        Pc = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //水平入力と垂直入力から入力方向を調べる.
+        Vector2 InputDirection;
+        InputDirection.x = Input.GetAxis("Horizontal");
+        InputDirection.y = Input.GetAxis("Vertical");
+        //入力が0でなければ動く.
+        if (InputDirection.magnitude > 0)
+        {
+            Pc.transform.position = new Vector2(Pc.transform.position.x + (InputDirection.x/20), Pc.transform.position.y+0/*InputDirection.y*/);
+        }
+        //else { Pc.transform.position = new Vector2(0, 0); }
+        //Debug.Log(InputDirection.x + " , " + InputDirection.y);
+    }
+}
